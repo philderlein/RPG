bafe58cc930d8a4394acdfa8697f7f76be7b61db
Trostka <kyletrost@verizon.net>
2015-04-18 16:36:02 -0500

    So many things

---
+++ TimeShenanigans_2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Actor 
+{
+	//public bool enemyTurn;
+	private bool type;
+
+	public GameObject playerPosition;
+
+	//public int spawnNum;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		type = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*if(enemyTurn)
+		{
+			//Attack the player on their screen.
+			Attack ();
+			//enemyTurn = false;
+		}*/
+	}
+
+	void Attack()
+	{
+		//Attack the player that is on their target.
+		GameObject player = playerPosition.transform.GetChild(0).gameObject;
+		Player playerScript = player.GetComponent<Player>();
+		
+		playerScript.DamageType(10, type);
+	}
+}
---
+++ TimeShenanigans_2/Assets/Scripts/Player_Parent.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class Player_Parent : MonoBehaviour {
+
+
+	
+	public GameObject [] plyrArray;
+	public Turn_Handler script;
+	public Player position;
+
+	//set playerPosition based on place in array
+	void Start(){
+		for (int i = 0; i < plyrArray.Length; i++) {
+			Player position = plyrArray [i].GetComponent<Player>();
+			position.playerPosition = i;
+		}
+	}
+
+	//deals damage to future selves
+	public void DealFutureDamage (int target, int damage) {
+		for (; target < plyrArray.Length; target++) {
+			plyrArray [target].BroadcastMessage("Damage", damage);
+		}
+	}
+
+	//Deals damage to past selves
+	public void DealPastDamage (int target, int damage) {
+		for (; target < plyrArray.Length; target++) {
+			plyrArray [target].BroadcastMessage("Damage", damage);
+		}
+	}
+
+
+	//Change turn to next player or start players' turns if the enemy just ended their turn
+	public void NextTurn (){
+
+		//Iterate and essentially increment the turn
+		for (int i = 0; i < plyrArray.Length-1; i++) {
+			position = plyrArray [i].GetComponent<Player>();
+			if(position.turn){
+				position.turn = false;
+
+				position = plyrArray [i+1].GetComponent<Player>();
+				position.turn = true;
+				Debug.Log ("player " + i + "'s turn.");
+				return;
+			}
+		}
+		//If we get here, then we know the first 3 players' turns are false
+
+		//Check the last player's turn
+		position = plyrArray [plyrArray.Length].GetComponent<Player>();
+		if(position.turn){ //if the last player just did his turn
+			position.turn = false;
+			//Do enemy turns
+			script.FlipTurn ();
+		}
+		else{ //if None of the players have done a turn, meaning the enemies just finished their turn
+			position = plyrArray [0].GetComponent<Player>();
+			position.turn = true;
+		}
+	}
+
+
+
+	/*void Update () {
+		//Do nothing
+	}*/
+}
